4bbe135443182460bdcc4f25e54adf5987463396
HudaSale7 <hudasaleh640@gmail.com>
2022-09-03 03:18:59 +0200

    new prob

---
+++ cses/bitStrings/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main() {
+    long double num, base = 2;
+    const unsigned int m = 1000000007;
+    cin >> num;
+    long long int sumBits = 0;
+    for (long double i = 0; i < num; i++)
+    {
+        sumBits = sumBits + int(pow(base, i)) % m;
+    }
+    cout << ++sumBits << endl;
+    system("pause");
+    return 0;
+}
---
+++ cses/trailingZeros/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+long long int fac(long long int num) {
+    if (num != 0)
+        return num * fac(num - 1);
+    else
+        return 1;
+}
+int main() {
+    long long int num;
+    unsigned long long int facNum;
+    long long int numOfZeros = 0;
+    int counter = -1;
+    cin >> num;
+    facNum = fac(num);
+    while (numOfZeros == 0)
+    {
+        numOfZeros = facNum % 10;
+        facNum /= 10;
+        counter++;
+    }
+    cout << counter << endl;
+    system("pause");
+    return 0;
+}
